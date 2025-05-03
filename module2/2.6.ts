98fc8799e3b9c94c6e93268a37901022c733eecc
Anondo Ray <Anondo554@gmail.com>
2025-05-03 18:33:45 +0600

    update

--- module2/2.6.ts
+++ module2/2.6.ts
@@ -1,13 +1,44 @@
 {
-  //
+  //constranints
 
+  const addCourseToStudent = <
+    T extends { id: number; name: string; email: string }
+  >(
+    student: T
+  ) => {
+    const course = 'Next Level Web Development';
 
+    return {
+      ...student,
+      course,
+    };
+  };
 
+  const student1 = addCourseToStudent({
+    id: 1,
+    name: 'Mr Y',
+    email: 'z@gmail.com',
+    devType: 'NLWD',
+  });
 
+  const student2 = addCourseToStudent<{
+    id: number;
+    name: string;
+    email: string;
+    hasWatch: string;
+  }>({
+    id: 2,
+    name: 'Mr X',
+    email: 'Y@gmail.com',
+    hasWatch: 'Apple Watch',
+  });
 
-
-
-
+  const student3 = addCourseToStudent({
+    id: 3,
+    name: 'MR X',
+    email: 'r@gmail.com',
+    emni: 'emni',
+  });
 
   //
 }
