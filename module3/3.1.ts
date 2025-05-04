938e191d84ee23a184fbc9973939f73ebb8e35c8
Anondo Ray <Anondo554@gmail.com>
2025-05-04 23:14:48 +0600

    class update

--- module3/3.1.ts
+++ module3/3.1.ts
@@ -11,10 +11,14 @@
       this.species = species;
       this.sound = sound;
     }
+    makeSound() {
+      console.log(`The ${this.name} says ${this.sound}`);
+    }
   }
 
   const dog = new Amimal('German Shepard', 'dog', 'Ghe w Ghew');
   const cat = new Amimal('Persian', 'cat', 'miw miw');
+  cat.makeSound();
 
   //
 }
