03644a3a9feec41e9861495c322c026bdb904a5c
Anondo Ray <Anondo554@gmail.com>
2025-05-05 22:15:54 +0600

    update

--- module3/3.1.ts
+++ module3/3.1.ts
@@ -1,24 +1,23 @@
-{
-  //oop -class
+// OOP - ক্লাস (Class) ব্যবহার করে অবজেক্ট তৈরি
 
-  class Amimal {
-    name: string;
-    species: string;
-    sound: string;
+class Animal {
+  // কনস্ট্রাক্টর এর মাধ্যমে প্রপার্টিগুলো ডিফাইন করা হয়েছে
+  // এখানে parameter properties ব্যবহার করা হয়েছে, তাই আলাদা করে প্রপার্টি ডিক্লেয়ার করার প্রয়োজন নেই
+  constructor(
+    public name: string, // প্রাণীর নাম
+    public species: string, // প্রাণীর প্রজাতি
+    public sound: string // প্রাণীর ডাক
+  ) {}
 
-    constructor(name: string, species: string, sound: string) {
-      this.name = name;
-      this.species = species;
-      this.sound = sound;
-    }
-    makeSound() {
-      console.log(`The ${this.name} says ${this.sound}`);
-    }
+  // মেথডঃ প্রাণীর শব্দ (sound) কনসোলে দেখানোর জন্য
+  makeSound() {
+    console.log(`The ${this.name} says ${this.sound}`);
   }
-
-  const dog = new Amimal('German Shepard', 'dog', 'Ghe w Ghew');
-  const cat = new Amimal('Persian', 'cat', 'miw miw');
-  cat.makeSound();
-
-  //
 }
+
+// ক্লাস থেকে অবজেক্ট তৈরি
+const dog = new Animal('German Shepard', 'dog', 'Ghew Ghew');
+const cat = new Animal('Persian', 'cat', 'miw miw');
+
+// ক্যাট অবজেক্ট এর মেথড কল করা
+cat.makeSound();
