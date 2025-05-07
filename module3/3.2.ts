bd3c52c9bb546e55a67ebda378980340bb485cac
Anondo Ray <Anondo554@gmail.com>
2025-05-07 21:01:56 +0600

    update

--- module3/3.2.ts
+++ module3/3.2.ts
@@ -18,5 +18,33 @@
   const result1 = add(3, 5);
   console.log(result1);
 
+  //iN guard
+
+  type NormalUser = {
+    name: string;
+  };
+  type AdminUser = {
+    name: string;
+    role: 'admin';
+  };
+
+  const getUser = (user: NormalUser | AdminUser) => {
+    if ('role' in user) {
+      console.log(`My name is ${user.name} my rool is ${user.role}`);
+    } else {
+      console.log(`I am normal user. My name is ${user.name}`);
+    }
+  };
+
+  const normalUser: NormalUser = {
+    name: 'mr Normal vai',
+  };
+  const adminUser: AdminUser = {
+    name: 'mr Admin vai',
+    role: 'admin',
+  };
+
+  getUser(adminUser);
+
   //
 }
